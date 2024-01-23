70c774a6d472ba78bab8b2bf493f7101a83d75a2
MartinBaGar <martbari.g@gmail.com>
2024-01-23 23:50:06 +0100

    a

--- env/lib/gcc/x86_64-conda-linux-gnu/13.2.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../configure --prefix=/home/mabagar/etudes/masters/Master_ISDD_fiches/env --with-slibdir=/home/mabagar/etudes/masters/Master_ISDD_fiches/env/lib --libdir=/home/mabagar/etudes/masters/Master_ISDD_fiches/env/lib --mandir=/home/mabagar/etudes/masters/Master_ISDD_fiches/env/man --build=x86_64-conda-linux-gnu --host=x86_64-conda-linux-gnu --target=x86_64-conda-linux-gnu --enable-default-pie --enable-languages=c,c++,fortran,objc,obj-c++ --enable-__cxa_atexit --disable-libmudflap --enable-libgomp --disable-libssp --enable-libquadmath --enable-libquadmath-support --enable-libsanitizer --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-bootstrap --disable-multilib --enable-long-long --with-sysroot=/home/mabagar/etudes/masters/Master_ISDD_fiches/env/x86_64-conda-linux-gnu/sysroot --with-build-sysroot=/home/conda/feedstock_root/build_artifacts/gcc_compilers_1699751170916/_build_env/x86_64-conda-linux-gnu/sysroot --with-gxx-include-dir=/home/mabagar/etudes/masters/Master_ISDD_fiches/env/x86_64-conda-linux-gnu/include/c++/13.2.0 --with-pkgversion='conda-forge gcc 13.2.0-3' --with-bugurl=https://github.com/conda-forge/ctng-compilers-feedstock/issues/new/choose";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
