e3b1dbdd5b7a07b46cfb1511c39f863929e9b344
MartinBaGar <105659826+MartinBaGar@users.noreply.github.com>
2024-01-24 01:18:38 +0000

    &

---
+++ env/lib/R/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 262913
+#define R_NICK "Beagle Scouts"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "4"
+#define R_MINOR  "3.1"
+#define R_STATUS ""
+#define R_YEAR   "2023"
+#define R_MONTH  "06"
+#define R_DAY    "16"
+#define R_SVN_REVISION 84548
+#define R_FILEVERSION    4,31,84548,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
